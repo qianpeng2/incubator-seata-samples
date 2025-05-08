5e21dcdcf0dad56d3e063f185dabc9279ac6d0b4
peng.qian <qianpengyx@163.com>
2025-05-08 16:31:50 +0800

    fixed: transaction rollback will be failed

--- at-sample/springboot-seata/src/main/java/org/apache/seata/SpringbootSeataApplication.java
+++ at-sample/springboot-seata/src/main/java/org/apache/seata/SpringbootSeataApplication.java
@@ -2,6 +2,7 @@ package org.apache.seata;
 
 import org.apache.seata.e2e.E2EUtil;
 import org.apache.seata.service.BusinessService;
+import org.apache.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -9,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// demo 代码又问题,抛出异常的时候没有回滚,且不抛异常的情况下,数据也不一致
+@EnableAutoDataSourceProxy// 没有使用的情况下,seata 无法拦截SQL操作,无法在二阶段回滚
 public class SpringbootSeataApplication implements BeanFactoryAware {
 
 	private static BeanFactory BEAN_FACTORY;
